5119e57e2190f23ba300cae5102509abab3e3774
Peeranont Jamornmarn <peeranont.ja@tnis.com>
2018-12-08 11:03:30 +0700

    Change Primary color

---
+++ app/src/main/java/com/kku/pharm/project/dmathere/AlarmActivity.kt
@@ -0,0 +1,12 @@
+package com.kku.pharm.project.dmathere
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AlarmActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alarm)
+    }
+}
