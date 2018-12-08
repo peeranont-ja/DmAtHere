e0c845cfa368d11ce11e0c5c836c357774e13b61
Peeranont Jamornmarn <peeranont.ja@tnis.com>
2018-12-08 12:02:00 +0700

    Add Icon to Pagers

--- app/src/main/java/com/kku/pharm/project/dmathere/AlarmActivity.kt
+++ app/src/main/java/com/kku/pharm/project/dmathere/AlarmActivity.kt
@@ -1,46 +1,56 @@
 package com.kku.pharm.project.dmathere
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.view.ViewPager
-import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
-import android.support.v7.widget.Toolbar
 import android.support.v4.app.FragmentPagerAdapter
-import com.kku.pharm.project.dmathere.R.id.toolbar
+import android.support.v4.view.ViewPager
+import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_alarm.*
 
 
 class AlarmActivity : AppCompatActivity() {
+
+    private val iconList = intArrayOf(
+            R.drawable.ic_sunrise,
+            R.drawable.ic_sun,
+            R.drawable.ic_sunset,
+            R.drawable.ic_night)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alarm)
 
-
         setSupportActionBar(toolbar)
-
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
 
         setupViewPager(viewpager)
 
+
         tabs.setupWithViewPager(viewpager)
+        setupTabIcons()
+    }
+
+    private fun setupTabIcons() {
+        tabs.getTabAt(0)!!.setIcon(iconList[0])
+        tabs.getTabAt(1)!!.setIcon(iconList[1])
+        tabs.getTabAt(2)!!.setIcon(iconList[2])
+        tabs.getTabAt(3)!!.setIcon(iconList[3])
     }
 
     private fun setupViewPager(viewPager: ViewPager) {
         val adapter = ViewPagerAdapter(supportFragmentManager)
-        adapter.addFragment(AlarmMorningFragment(), "เช้า")
-        adapter.addFragment(AlarmNoonFragment(), "กลางวัน")
-        adapter.addFragment(AlarmEveningFragment(), "เย็น")
-        adapter.addFragment(AlarmNightFragment(), "ก่อนนอน")
+        adapter.addFrag(AlarmMorningFragment(), "เช้า")
+        adapter.addFrag(AlarmNoonFragment(), "กลางวัน")
+        adapter.addFrag(AlarmEveningFragment(), "เย็น")
+        adapter.addFrag(AlarmNightFragment(), "ก่อนนอน")
         viewPager.adapter = adapter
     }
 
     internal inner class ViewPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager) {
-        private val mFragmentList: ArrayList<Fragment> = ArrayList()
+        private val mFragmentList:ArrayList<Fragment> = ArrayList()
         private val mFragmentTitleList: ArrayList<String> = ArrayList()
 
         override fun getItem(position: Int): Fragment {
@@ -51,13 +61,15 @@ class AlarmActivity : AppCompatActivity() {
             return mFragmentList.size
         }
 
-        fun addFragment(fragment: Fragment, title: String) {
+        fun addFrag(fragment: Fragment, title: String) {
             mFragmentList.add(fragment)
             mFragmentTitleList.add(title)
         }
 
         override fun getPageTitle(position: Int): CharSequence? {
-            return mFragmentTitleList[position]
+//            return mFragmentTitleList[position]
+            return null
         }
     }
+
 }
