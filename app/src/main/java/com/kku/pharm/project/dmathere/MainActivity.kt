f5c519bb5982965486ebe794920d28bbdfc42888
Peeranont Jamornmarn <peeranont.ja@tnis.com>
2018-12-08 11:11:57 +0700

    Add intent

--- app/src/main/java/com/kku/pharm/project/dmathere/MainActivity.kt
+++ app/src/main/java/com/kku/pharm/project/dmathere/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.kku.pharm.project.dmathere
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupView()
+    }
+
+    private fun setupView(){
+        btn_alarm.setOnClickListener {
+            goToAlarmPage()
+        }
+    }
+
+    private fun goToAlarmPage(){
+        val intent = Intent(this, AlarmActivity::class.java)
+        startActivity(intent)
     }
 }
