f98c7e55e7633285719ad5fe8d4c1ef0b287b509
Peeranont Jamornmarn <peeranont.ja@tnis.com>
2018-12-08 22:17:35 +0700

    Add Spinner Library

--- app/src/main/java/com/kku/pharm/project/dmathere/AlarmActivity.kt
+++ app/src/main/java/com/kku/pharm/project/dmathere/AlarmActivity.kt
@@ -1,15 +1,13 @@
 package com.kku.pharm.project.dmathere
 
-import android.content.res.Resources
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_alarm.*
-import android.widget.TextView
 import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.activity_alarm.*
 
 class AlarmActivity : AppCompatActivity() {
 
--- app/src/main/java/com/kku/pharm/project/dmathere/AlarmMorningFragment.kt
+++ app/src/main/java/com/kku/pharm/project/dmathere/AlarmMorningFragment.kt
@@ -5,6 +5,12 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.fragment_alarm_morning.*
+import com.isapanah.awesomespinner.AwesomeSpinner
+
+
+
 
 class AlarmMorningFragment : Fragment() {
 
@@ -12,6 +18,44 @@ class AlarmMorningFragment : Fragment() {
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_alarm_morning, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupView()
+    }
+
+    private fun setupView() {
+        setupMedicineTypeSpinner()
+    }
+
+    private fun setupMedicineTypeSpinner(){
+        val categories =  arrayOf(
+                "NovoRapid Penfill",
+                "Insulatard Penfill",
+                "Actrapid Penfill",
+                "Mixtard Penfill",
+                "NovoMix",
+                "Lantus Solostar",
+                "Levemir Flexpen",
+                "Mixtard 30 HM",
+                "Actrapid HM",
+                "Insulatard HM"
+                )
+
+        val adapter = ArrayAdapter(context,android.R.layout.simple_spinner_item, categories)
+//        val categoriesAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, categories)
+
+        spinner_first_medicine.setAdapter(adapter)
+        spinner_second_medicine.setAdapter(adapter)
+
+        spinner_first_medicine.setOnSpinnerItemClickListener { position, itemAtPosition ->
+            //TODO YOUR ACTIONS
+        }
+
+        spinner_second_medicine.setOnSpinnerItemClickListener { position, itemAtPosition ->
+            //TODO YOUR ACTIONS
+        }
     }
 
 }
